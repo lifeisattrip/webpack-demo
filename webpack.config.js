1606866e26550e3e2c2904dc71c7b6ef7da1c79e
zyj <zyj@xwmail.com>
2020-08-26 21:55:59 +0800

    1、添加webpack-dev-server支持及npm启动命令 2、添加HtmlWebpackPlugin及相关配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 // webpack.config.js
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-    entry: './main.js',
+    //如果是多个js可以命名 并在后面指定加入哪个js
+    entry: {
+        main:'./main.js',
+        hello:'./hello.js'
+    },
     output: {
-      filename: 'bundle.js'
+      filename: '[name].js'
     },
     module: {
       //webpack使用loader的方式处理各种各样的资源
@@ -13,5 +20,31 @@ module.exports = {
           loader: 'babel-loader' //用babel-loader处理
       }
       ]
-  }
+  },
+    plugins: [
+        new HtmlWebpackPlugin({ // 打包输出HTML
+            title: '政企信息化 ',
+            minify: { // 压缩HTML文件
+                removeComments: true, // 移除HTML中的注释
+                collapseWhitespace: true, // 删除空白符与换行符
+                minifyCSS: true// 压缩内联css
+            },
+            filename: 'index.html', //生成的html文件名
+            template: 'index.html', //作为模板的html文件名 支持html、jade、ejs
+            hash: true,
+            chunks: ['main','hello']
+        }),
+        new HtmlWebpackPlugin({ // 打包输出HTML
+            title: '政企信息化2 ',
+            minify: { // 压缩HTML文件
+                removeComments: true, // 移除HTML中的注释
+                collapseWhitespace: true, // 删除空白符与换行符
+                minifyCSS: true// 压缩内联css
+            },
+            filename: 'index2.html', //生成的html文件名
+            template: 'index.html', //作为模板的html文件名 支持html、jade、ejs
+            hash: true,
+            chunks: ['hello']
+        })
+    ]
   };
