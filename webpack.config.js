c473381abb0df6eaf82b4d477a7816a4cbb00351
zyj <zyj@xwmail.com>
2020-08-26 22:46:53 +0800

    feature:1、添加clean webpack plugin 2、添加sourcemap配置 3、添加path配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,41 @@
 // webpack.config.js
 
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
 
 module.exports = {
     //如果是多个js可以命名 并在后面指定加入哪个js
     entry: {
-        main:'./main.js',
-        hello:'./hello.js'
+        main: './main.js',
+        hello: './hello.js'
     },
     output: {
-      filename: '[name].js'
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
-      //webpack使用loader的方式处理各种各样的资源
-      rules: [
-        {
-          test: /\.js$/, //处理以.js结尾的文件
-          exclude: /node_modules/, //处理除了nodde_modules里的js文件
-          loader: 'babel-loader' //用babel-loader处理
-      }
-      ]
-  },
+        //webpack使用loader的方式处理各种各样的资源
+        rules: [
+            {
+                test: /\.js$/, //处理以.js结尾的文件
+                exclude: /node_modules/, //处理除了nodde_modules里的js文件
+                loader: 'babel-loader' //用babel-loader处理
+            }
+        ]
+    },
+    //sourcemap配置
+    optimization: {    // 1. 这个配置必须
+        minimize: false
+    },
+    devtool: "source-map", // 2. 这个配置必须
+
+
     plugins: [
+        //新的用法
+        // All files inside webpack's output.path directory will be removed once
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({ // 打包输出HTML
             title: '政企信息化 ',
             minify: { // 压缩HTML文件
@@ -32,7 +46,7 @@ module.exports = {
             filename: 'index.html', //生成的html文件名
             template: 'index.html', //作为模板的html文件名 支持html、jade、ejs
             hash: true,
-            chunks: ['main','hello']
+            chunks: ['main', 'hello']
         }),
         new HtmlWebpackPlugin({ // 打包输出HTML
             title: '政企信息化2 ',
@@ -47,4 +61,4 @@ module.exports = {
             chunks: ['hello']
         })
     ]
-  };
+};
